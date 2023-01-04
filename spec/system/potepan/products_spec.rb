e17ec168ac2cd1cfb7435fff22e3d18dd59c15e8
amaco1489 <116137741+amaco1489@users.noreply.github.com>
2023-01-04 09:43:38 +0900

    fix: 不要コード・空行削除、zero?メソッド追加等

--- spec/system/potepan/products_spec.rb
+++ spec/system/potepan/products_spec.rb
@@ -14,14 +14,17 @@ RSpec.describe "Potepan::Products", type: :system do
       expect(page).to have_content product.display_price
       expect(page).to have_content product.description
     end
+
     it "ヘッダーのHomeボタンからtopページへ遷移できるか" do
       click_on "header-home-btn"
       expect(current_path).to eq potepan_index_path
     end
+
     it "ヘッダーのロゴからtopページへ遷移できるか" do
       click_on "ロゴ"
       expect(current_path).to eq potepan_index_path
     end
+
     it "lightSectionのHomeボタンからtopページへ遷移できるか" do
       click_on "light-section-home-btn"
       expect(current_path).to eq potepan_index_path
