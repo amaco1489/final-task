562467f50f3665194a677686c4b9454f929ac771
amaco1489 <116137741+amaco1489@users.noreply.github.com>
2023-01-15 14:12:28 +0900

    fix: N+1解消のための修正等

--- app/controllers/potepan/categories_controller.rb
+++ app/controllers/potepan/categories_controller.rb
@@ -2,6 +2,7 @@ class Potepan::CategoriesController < ApplicationController
   def show
     @taxon = Spree::Taxon.find(params[:id])
     @taxonomies = Spree::Taxonomy.all
-    @products = @taxon.all_products.preload(master: [:images, :default_price])
+    @products =
+      @taxon.products.preload(master: [:default_price, images: { attachment_attachment: :blob }])
   end
 end
