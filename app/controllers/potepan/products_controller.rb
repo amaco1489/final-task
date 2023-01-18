77a5300bb39f2d515d8e0ebda9a849ad1d13fd48
amaco1489 <116137741+amaco1489@users.noreply.github.com>
2023-01-18 17:42:29 +0900

    add: decorator導入、及び関連商品取得メソッドの定義

--- app/controllers/potepan/products_controller.rb
+++ app/controllers/potepan/products_controller.rb
@@ -2,5 +2,7 @@ class Potepan::ProductsController < ApplicationController
   def show
     @product = Spree::Product.find(params[:id])
     @images = @product.images
+    @related_products = @product.related_products.preload(master:
+      [:default_price, images: { attachment_attachment: :blob }]).order("RAND()").limit(4)
   end
 end
