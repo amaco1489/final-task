01c530cea986050fe821fc6a8c48824f313ae541
amaco1489 <116137741+amaco1489@users.noreply.github.com>
2023-01-20 06:09:00 +0900

    fix: 表示する関連商品数を定数に変更

--- app/controllers/potepan/products_controller.rb
+++ app/controllers/potepan/products_controller.rb
@@ -1,8 +1,10 @@
 class Potepan::ProductsController < ApplicationController
+  RELATED_PRODUCTS_LIMIT = 4
+
   def show
     @product = Spree::Product.find(params[:id])
     @images = @product.images
     @related_products = @product.related_products.preload(master:
-      [:default_price, images: { attachment_attachment: :blob }]).limit(4)
+      [:default_price, images: { attachment_attachment: :blob }]).limit(RELATED_PRODUCTS_LIMIT)
   end
 end
